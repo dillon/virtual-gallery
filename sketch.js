8135dcd9b930272e0a9d2065f325bfed497ba7d0
dpett122 <Dillon@172-30-48-201.DYNAPOOL.NYU.EDU>
2015-12-08 22:57:08 -0500

    delete rotateY

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ function draw() {
   var radius = width * 1.5;
   //camera
   perspective(60 / 180 * PI, width / height, .1, 100)
-  rotateY(accelerationX * 0.05);
   rotateX(accelerationY * 0.05);
+  //rotateY(accelerationX * 0.05);
   //rotateZ(accelerationZ * 0.05);
 
   normalMaterial();
