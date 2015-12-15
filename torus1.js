8dcdcac4a5fc81af97de7ada80b17f9efd33448a
dpett122 <Dillon@Dillons-MacBook-Pro.local>
2015-12-15 02:08:06 -0500

    really out on a limb

---
+++ torus1.js
@@ -0,0 +1,34 @@
+{
+	"metadata": {
+		"version": 4.4,
+		"type": "Object",
+		"generator": "Object3D.toJSON"
+	},
+	"geometries": [
+		{
+			"uuid": "07EB163C-E154-447B-AC9D-C5AE9D9FCD02",
+			"type": "TorusGeometry",
+			"radius": 100,
+			"tube": 40,
+			"radialSegments": 8,
+			"tubularSegments": 6,
+			"arc": 6.283185307179586
+		}],
+	"materials": [
+		{
+			"uuid": "61A14919-C14A-43AC-ACDE-BFCFD36A181E",
+			"type": "MeshPhongMaterial",
+			"color": 16777215,
+			"emissive": 0,
+			"specular": 1118481,
+			"shininess": 30
+		}],
+	"object": {
+		"uuid": "83D4AF08-FE41-4F49-A895-BC7A8701F360",
+		"type": "Mesh",
+		"name": "Torus 2",
+		"matrix": [1,0,0,0,0,1,0,0,0,0,1,0,0,0,-379.2117614746094,1],
+		"geometry": "07EB163C-E154-447B-AC9D-C5AE9D9FCD02",
+		"material": "61A14919-C14A-43AC-ACDE-BFCFD36A181E"
+	}
+}
